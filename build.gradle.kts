72f2058660806581fe5b6f2050a563617ff5fb0b
Geoffrey Challen <challen@illinois.edu>
2023-11-10 19:53:42 -0600

    Publishing nonsense.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,15 @@ plugins {
     kotlin("jvm") version "1.9.10"
     `java-gradle-plugin`
     `maven-publish`
+    signing
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("org.jmailen.kotlinter") version "3.16.0"
     id("com.github.ben-manes.versions") version "0.48.0"
+    id("io.github.gradle-nexus.publish-plugin") version "1.3.0"
 }
 
-group = "com.github.cs124-illinois"
-version = "2023.10.0"
+group = "org.cs124"
+version = "2023.11.0"
 
 repositories {
     mavenCentral()
@@ -22,15 +24,11 @@ dependencies {
 }
 gradlePlugin {
     plugins {
-        create("plugin") {
-            id = "com.github.cs124-illinois.gradleoverlay"
+        create("gradleoverlay") {
+            id = "org.cs124.gradleoverlay"
             implementationClass = "edu.illinois.cs.cs125.gradleoverlay.Plugin"
         }
     }
-}
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
 }
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
@@ -45,4 +43,58 @@ tasks.dependencyUpdates {
     }
     gradleReleaseChannel = "current"
 }
-
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+    withJavadocJar()
+    withSourcesJar()
+}
+publishing {
+    publications {
+        afterEvaluate {
+            withType<MavenPublication> {
+                pom {
+                    name = "gradleoverlay"
+                    description = "Gradle overlay plugin for CS 124."
+                    url = "https://cs124.org"
+                    licenses {
+                        license {
+                            name = "MIT License"
+                            url = "https://opensource.org/license/mit/"
+                        }
+                    }
+                    developers {
+                        developer {
+                            id = "gchallen"
+                            name = "Geoffrey Challen"
+                            email = "challen@illinois.edu"
+                        }
+                    }
+                    scm {
+                        connection = "scm:git:https://github.com/cs124-illinois/gradleoverlay.git"
+                        developerConnection = "scm:git:https://github.com/cs124-illinois/gradleoverlay.git"
+                        url = "https://github.com/cs124-illinois/gradleoverlay"
+                    }
+                }
+                signing {
+                    sign(this@publications)
+                }
+            }
+        }
+    }
+}
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
+    }
+}
+tasks.withType<AbstractPublishToMaven>().configureEach {
+    val signingTasks = tasks.withType<Sign>()
+    mustRunAfter(signingTasks)
+}
